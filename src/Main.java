db9958e039533d2811a94b7cb08b83194400fbe1
Maks2305sv <salnojgribmaksim@gmail.com>
2025-10-27 16:34:15 +0300

    4-тий раз пушу

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world from task 3");
+        System.out.println("hello world from task 4");
     }
 }
